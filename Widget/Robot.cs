e1e6f5cd6ab855e21b904f3f8c807ef7a4ab220e
chou <Calvin.Hou@elbitsystems-us.com>
2023-01-27 06:44:11 -0800

    Initial Commit. EventLog throws in python.

---
+++ Widget/Robot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Widget
+{
+    public class Robot
+    {
+        public EventLog CreateEventLog()
+        {
+            return new EventLog();
+        }
+    }
+}
