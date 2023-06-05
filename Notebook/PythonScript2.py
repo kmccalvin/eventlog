56467a8ea1bc47212826a9f12c26a43256bd2304
kmccalvin <120133088+kmccalvin@users.noreply.github.com>
2023-06-05 10:29:48 -0400

    Add Robot Creator (#2)

---
+++ Notebook/PythonScript2.py
@@ -0,0 +1,18 @@
+from pythonnet import load
+load("coreclr")
+import sys
+import os
+import platform
+sys.path.append(os.path.abspath(r"../Widget/bin/Debug/net6.0"))
+
+import clr
+clr.AddReference("Widget")
+
+from Widget import RobotCreator
+
+print(os.name)
+print(platform.system())
+print(platform.release())
+
+robotCreator = RobotCreator()
+robot = robotCreator.CreateRobot()
---
+++ Widget/RobotCreator.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Widget
+{
+    public class RobotCreator
+    {
+        public Robot CreateRobot()
+        {
+            IHost host = Host.CreateDefaultBuilder()
+                .ConfigureServices((_, services) =>
+                {
+                    services
+                        .AddSingleton<Robot>();
+                })
+                .Build();
+            Robot robot = host.Services.GetRequiredService<Robot>();
+            return robot;
+        }
+    }
+}
