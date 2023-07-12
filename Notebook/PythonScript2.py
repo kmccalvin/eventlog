2479add203f291a8aed37fa21ba5e8a79b4845a5
Hou <Calvin.Hou@kmc.elbitsystems-us.com>
2023-07-12 10:08:15 -0400

    Try specifying windows in target

--- Notebook/PythonScript2.py
+++ Notebook/PythonScript2.py
@@ -3,7 +3,7 @@ load("coreclr")
 import sys
 import os
 import platform
-sys.path.append(os.path.abspath(r"../Widget/bin/Debug/net6.0"))
+sys.path.append(os.path.abspath(r"../Widget/bin/Debug/net6.0-windows10.0.22621.0"))
 
 import clr
 clr.AddReference("Widget")
